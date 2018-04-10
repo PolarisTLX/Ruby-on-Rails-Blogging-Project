d5780a5908b8e8024067cc2a93c6baecb8840c47
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-04-10 11:52:43 -0400

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # this makes the landing page be localhost:3000/articles
   # instead of just localhost:3000  (don't need to add  the /articles anymore)
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
