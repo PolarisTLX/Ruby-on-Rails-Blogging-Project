164461795e0cc99d196ed0836b921630becde835
PolarisTLX <paul.a.rail@gmail.com>
2018-04-10 10:52:54 -0400

    create edit delete and flash notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 # Blogger::Application.routes.draw do  # old convention for Rails 4
 Rails.application.routes.draw do
+  # this makes the landing page be localhost:3000/articles
+  # instead of just localhost:3000  (don't need to add  the /articles anymore)
+  root to: 'articles#index'
   resources :articles
 end
